b9224305201b85e9e112923b4b3e94a45a115e59
Mindatsdev <enirayetankehinde@gmail.com>
2023-02-05 04:29:16 -0800

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'C:/Users/User56/Desktop/New_DJP/mysite/blog/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
